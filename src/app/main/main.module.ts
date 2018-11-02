5e9bb235bb472cfb33a402ab8bd6be1bcca38819
paraelle <paraelle@MacBook-Air-user.local>
2018-11-02 13:21:12 +0100

    main, button and icon components added to project

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './main.component'
+import { SharedModule } from '../shared/shared.module'
+
+@NgModule({
+  declarations: [
+    MainComponent,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  bootstrap: [ MainComponent ],
+  exports: [ MainComponent ]
+})
+export class MainModule { }
